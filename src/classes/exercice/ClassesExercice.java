b1f0b7942f8aabf014b6cc907a17e849c50c00e1
mameastagueye229-netizen <mameastagueye229@gmail.com>
2026-04-07 13:50:00 +0200

    first commit

---
+++ src/classes/exercice/ClassesExercice.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classes.exercice;
+
+/**
+ *
+ * @author DELL
+ */
+public class ClassesExercice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
