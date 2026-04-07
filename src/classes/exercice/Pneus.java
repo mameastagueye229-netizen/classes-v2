fea6d2fa896752696e769a05962173e34588d889
mameastagueye229-netizen <mameastagueye229@gmail.com>
2026-04-07 16:40:09 +0200

    mission 6

---
+++ src/classes/exercice/Pneus.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes.exercice;
+
+/**
+ *
+ * @author DELL
+ */
+public class Pneus {
+    int largeur;
+    int hauteur;
+    int diametre;
+    String type;
+
+    public Pneus(int largeur, int hauteur, int diametre, String type) {
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.diametre = diametre;
+        this.type = type;
+     }   
+}
