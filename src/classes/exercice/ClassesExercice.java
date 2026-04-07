567292d0531841ed26ef37b225ad60b7f52c0840
mameastagueye229-netizen <mameastagueye229@gmail.com>
2026-04-07 15:37:11 +0200

    mission 5

--- src/classes/exercice/ClassesExercice.java
+++ src/classes/exercice/ClassesExercice.java
@@ -21,16 +21,22 @@ public class ClassesExercice {
         int annee = 2021;
         double prix = 14500.0;
         System.out.println(" " + marque + " " + modele + " de " + annee + " au prix de " + prix );
-          */
-       Voiture v1 = new Voiture();
+         
+      Voiture v1 = new Voiture();
 v1.marque = "Peugeot"; v1.modele = "208"; v1.annee = 2021; v1.prix = 14500;
 
 Voiture v2 = new Voiture();
 v2.marque = "Renault"; v2.modele = "Clio"; v2.annee = 2000; v2.prix = 3500;
-
 Voiture v3 = new Voiture();
 v3.marque = "Toyota"; v3.modele = "Yaris"; v3.annee = 2015; v3.prix = 9800;
  System.out.println("" + v1.marque + " " + v1.modele + " (" + v1.annee + ") - " + v1.prix );
+System.out.println(" " + v2.marque + " " + v2.modele + " (" + v2.annee + ") - " + v2.prix );
+System.out.println(" " + v3.marque + " " + v3.modele + " (" + v3.annee + ") - " + v3.prix);
+  */
+         Voiture v1 = new Voiture("Peugeot", "208", 2021, 14500);
+         Voiture v2 = new Voiture("Renault", "clio", 2000, 3500);
+         Voiture v3 = new Voiture("Toyota", "Yaris", 2015, 9800);
+         System.out.println("" + v1.marque + " " + v1.modele + " (" + v1.annee + ") - " + v1.prix );
 System.out.println(" " + v2.marque + " " + v2.modele + " (" + v2.annee + ") - " + v2.prix );
 System.out.println(" " + v3.marque + " " + v3.modele + " (" + v3.annee + ") - " + v3.prix);
     }
--- src/classes/exercice/Voiture.java
+++ src/classes/exercice/Voiture.java
@@ -14,4 +14,12 @@ public class Voiture {
     String modele;
     int annee;
     double prix;
+    
+    public Voiture(String marque, String modele, int annee, double prix) {
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+        this.prix = prix;
+}
+    
 }
